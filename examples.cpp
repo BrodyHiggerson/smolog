e721066cd8f177f00b945f749ec1730be75f90f8
BrodyHiggerson <7245990+BrodyHiggerson@users.noreply.github.com>
2020-12-28 21:39:03 +1100

    Disabled smolog examples and fixed build

--- examples.cpp
+++ examples.cpp
@@ -15,13 +15,13 @@ int main() {
 	main_logger.add_sink(stderr_sink);
 	
 	// now we can write to the logger by calling either a function that specifies the level
-	main_logger.debug("Hello Smolog!");
+	main_logger.smolog_debug("Hello Smolog!");
 
 	// let's remove the stderr sink for less clutter
 	main_logger.remove_sink(stderr_sink.get());
 
 	// you can also specify the log level via log_at
-	main_logger.log_at(smolog::Level::Error, "Oh-oh something went wrong :(");
+	main_logger.smolog_log_at(smolog::Level::Error, "Oh-oh something went wrong :(");
 
 	// these are *not* member function, even though they look like that
 	// unfortunately some macro hacks are required to check the arguments you pass in
@@ -29,7 +29,7 @@ int main() {
 	// (you can disable the macros via CMake)
 
 	// arguments to the logging functions are standard format strings, passed on to snprintf
-	main_logger.info("My friend %s is %d years old", "Martin", 24);
+	main_logger.smolog_info("My friend %s is %d years old", "Martin", 24);
 	
 	// via the macros the compiler checks what you do
 	// main_logger.critical("Floating away to %d", 4.2f); -> warning!
@@ -40,7 +40,7 @@ int main() {
 #ifdef _MSC_VER
 	// for example in MSVC we can log to the debug output
 	main_logger.add_sink(std::make_shared<smolog::msvc_sink>());
-	main_logger.error("Good luck with smolog!")
+	main_logger.smolog_error("Good luck with smolog!")
 #endif
 
 	return 0;
